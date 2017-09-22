c772c4cb0fbd154912df1754627346ee15c8ab0f
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-09-22 18:16:09 -0400

    Added playbooks/scripts for PDNS
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

---
+++ scripts/vsphere_management.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_management
+
+ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_bootstrap_vms
+
+ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_powerdns_vms
+
+ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml
+
+ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_dns
