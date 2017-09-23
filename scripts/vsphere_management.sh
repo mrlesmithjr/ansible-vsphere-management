c7b21b00e2dfe6b29268b4a812182a7c42627183
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-09-23 11:59:19 -0400

    Added Keepalived VIPs to be used for DNS and etc. on PDNS VMs
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- scripts/vsphere_management.sh
+++ scripts/vsphere_management.sh
@@ -6,8 +6,12 @@ ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_b
 
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_powerdns_vms
 
+ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml --tags vsphere_powerdns_vms_info
+
 ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml
 
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_dns
+
+ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml --tags vsphere_powerdns_vms_info
 
 ansible-playbook -i inventory/ playbooks/pdns.yml
