1a8b440d86264478c75dd9b04b4b0b07390112fe
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-09-23 12:19:36 -0400

    Enabled logging
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- scripts/vsphere_management.sh
+++ scripts/vsphere_management.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
+LOG_DIR=logs
+
+if [ ! -d $LOG_DIR ]; then
+  mkdir $LOG_DIR
+else [ -d $LOG_DIR ]
+  echo "$LOG_DIR already exists"
+fi
+
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_management
 
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_bootstrap_vms
