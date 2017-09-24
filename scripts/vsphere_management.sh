88aa2c3e451710dbc5680829f64b3f20c6584392
Larry Smith Jr <mrlesmithjr@gmail.com>
2017-09-23 23:05:24 -0400

    First commit of load balancing for core services
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- scripts/vsphere_management.sh
+++ scripts/vsphere_management.sh
@@ -20,12 +20,20 @@ ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_b
 
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_powerdns_vms
 
+ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_lb_vms
+
 ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml --tags vsphere_powerdns_vms_info
 
+ansible-playbook -i inventory/ playbooks/vsphere_lb.yml --tags vsphere_lb_vms_info
+
 ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml
+
+ansible-playbook -i inventory/ playbooks/vsphere_lb.yml
 
 ansible-playbook -i inventory/ playbooks/vsphere_management.yml --tags vsphere_dns
 
 ansible-playbook -i inventory/ playbooks/vsphere_powerdns.yml --tags vsphere_powerdns_vms_info
+
+ansible-playbook -i inventory/ playbooks/vsphere_lb.yml --tags vsphere_lb_vms_info
 
 ansible-playbook -i inventory/ playbooks/pdns.yml
